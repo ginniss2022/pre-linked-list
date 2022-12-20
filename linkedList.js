f100fb66581fd381310738c6d096e313f257af8e
ginniss2022 <gabevaughninn@gmail.com>
2022-12-20 21:28:37 +0000

    1

--- linkedList.js
+++ linkedList.js
@@ -2,13 +2,14 @@
 
 class Node {
     constructor(data = null){
-
+        this.data = data;
+        this.next = null
     }
 }
 
 class LinkedList {
     constructor() {
-
+    
     }
     
     appendToTail(data){ 
